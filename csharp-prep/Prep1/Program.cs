082706da6b8b5caf995f4dc9c90a8d49ba8e4026
mcmonde <mcmeliton1889@gmail.com>
2024-04-28 11:03:36 +0800

    prep 1 done.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -10,6 +10,6 @@ class Program
         Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last}, {first_name} {last_name}.");
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
